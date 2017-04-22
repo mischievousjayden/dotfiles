fdc3661baed3608a54220506888d2e22e0df519a
Jayden <mischievousjayden@gmail.com>
2017-04-22 10:01:25 -0400

    fixed wrong file path

--- vimsetting/vim_init.sh
+++ vimsetting/vim_init.sh
@@ -5,12 +5,12 @@ ln -s $PWD/vim $HOME/.vim
 ln -s $PWD/vim/vimrc $HOME/.vimrc
 
 # install pathogen
-mkdir -p ~/.vim/autoload
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p $HOME/.vim/autoload
+curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # install vim bundle 
 git submodule update --init --recursive
 
 # youcompleteme install
-cd vim/bundle/youcompleteme && ./install.py --clang-completer --system-libclang --gocode-completer --tern-completer
+cd $HOME/.vim/bundle/youcompleteme && ./install.py --clang-completer --system-libclang --gocode-completer --tern-completer
 
