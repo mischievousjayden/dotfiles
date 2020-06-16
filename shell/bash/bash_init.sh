673e44755943d8d34c8f8e38eebf5671ba699915
jayden <mischievousjayden@gmail.com>
2020-06-15 22:13:03 -0700

    update bashrc with common function

--- shell/bash/bash_init.sh
+++ shell/bash/bash_init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# link config
 [ -f "$HOME"/.bashrc ] && mv "$HOME"/.bashrc "$HOME"/.bashrc.backup
 ln -s "$PWD"/bashrc ~/.bashrc
 
-[ -f "$HOME"/.bash_aliases ] && mv "$HOME"/.bash_aliases "$HOME"/.bash_aliases.backup
-ln -s "$PWD"/bash_aliases ~/.bash_aliases
+[ -f "$HOME"/.bash_function ] && mv "$HOME"/.bash_function "$HOME"/.bash_function.backup
+ln -s "$PWD"/../common/common_function "$HOME"/.bash_function
+
+[ -f "$HOME"/.bash_alias ] && mv "$HOME"/.bash_alias "$HOME"/.bash_alias.backup
+ln -s "$PWD"/../common/common_alias "$HOME"/.bash_alias
 
--- shell/zsh/zsh_init.sh
+++ shell/zsh/zsh_init.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 
-# link config
-ln -s $PWD/zshrc ${ZDOTDIR:-$HOME}/.zshrc
-ln -s $PWD/../common/common_alias ${ZDOTDIR:-$HOME}/.zsh_alias
-ln -s $PWD/../common/common_function ${ZDOTDIR:-$HOME}/.zsh_function
+[ -f "${ZDOTDIR:-$HOME}"/.zshrc ] && mv "${ZDOTDIR:-$HOME}"/.zshrc "${ZDOTDIR:-$HOME}"/.zshrc.backup
+ln -s "$PWD"/zshrc "${ZDOTDIR:-$HOME}"/.zshrc
+
+[ -f "${ZDOTDIR:-$HOME}"/.zsh_function ] && mv "${ZDOTDIR:-$HOME}"/.zsh_function "${ZDOTDIR:-$HOME}"/.zsh_function.backup
+ln -s "$PWD"/../common/common_function "${ZDOTDIR:-$HOME}"/.zsh_function
+
+[ -f "${ZDOTDIR:-$HOME}"/.zsh_alias ] && mv "${ZDOTDIR:-$HOME}"/.zsh_alias "${ZDOTDIR:-$HOME}"/.zsh_alias.backup
+ln -s "$PWD"/../common/common_alias "${ZDOTDIR:-$HOME}"/.zsh_alias
 
