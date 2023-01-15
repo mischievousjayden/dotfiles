4d33f3eb149d8cfd4779fb30f7bb57667ae4cb21
jayden <mischievousjayden@gmail.com>
2023-01-15 00:36:25 -0800

    setup for vim8

--- vim/vim_init.sh
+++ vim/vim_init.sh
@@ -5,11 +5,7 @@
 ln -s "$PWD"/vim "$HOME"/.vim
 
 [ -f "$HOME"/.vimrc ] && mv "$HOME"/.vimrc "$HOME"/.vimrc.backup
-ln -s "$PWD"/vim/vimrc "$HOME"/.vimrc
-
-# install pathogen
-mkdir -p $HOME/.vim/autoload
-curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+ln -s "$PWD"/vimrc "$HOME"/.vimrc
 
 # install vim bundle 
 git submodule update --init --recursive
