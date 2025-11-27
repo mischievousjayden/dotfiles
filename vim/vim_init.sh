8ae2f2cac80ffd2965f1a31265d64b98cb21f204
Jayden <mischievousjayden@gmail.com>
2025-11-26 18:29:12 -0800

    remove minpac and add tcomment

--- vim/vim_init.sh
+++ vim/vim_init.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-# link config
-[ -f "$HOME"/.vim ] && mv "$HOME"/.vim "$HOME"/.vim.backup
-ln -s "$PWD"/vim "$HOME"/.vim
+# link config for vim
+VIM_CONFIG_PACK_DIR="$HOME"/.vim/pack
+[ -f "$VIM_CONFIG_PACK_DIR" ] && mv "$VIM_CONFIG_PACK_DIR" "$VIM_CONFIG_PACK_DIR".backup
+ln -s "$PWD"/pack "$VIM_CONFIG_DIR"
 
-[ -f "$HOME"/.vimrc ] && mv "$HOME"/.vimrc "$HOME"/.vimrc.backup
-ln -s "$PWD"/vimrc "$HOME"/.vimrc
+# link config for nvim
+NVIM_CONFIG_PACK_DIR="$HOME"/.config/nvim/pack
+[ -f "$NVIM_CONFIG_PACK_DIR" ] && mv "$NVIM_CONFIG_PACK_DIR" "$NVIM_CONFIG_PACK_DIR".backup
+ln -s "$PWD"/pack "$NVIM_CONFIG_DIR"
+
+# link vimrc
+VIMRC_FILE="$HOME"/.vimrc
+[ -f "$VIMRC_FILE" ] && mv "$VIMRC_FILE" "$VIMRC_FILE".backup
+ln -s "$PWD"/vimrc "$VIMRC_FILE"
 
 # install vim bundle 
 git submodule update --init --recursive
